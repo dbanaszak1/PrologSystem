f0ccd9fe08ba509acf077088e0923b90d49b5eed
Uni <hornung.bartek@gmail.com>
2024-04-12 20:37:56 +0200

    test1

--- baza.pl
+++ baza.pl
@@ -2,7 +2,7 @@
 showPokemonType(Pokemon) :-
     pokemontype(Pokemon, Type),
     write(Type).
-
+/*Orzel zmiana*/
 /* Define the predicate for checking effectiveness */
 effectiveAgainst(Pokemon, TargetType) :-
     pokemontype(Pokemon, Type),
